122a97bbc1ceeeb262c5aeb2d79dd5519f089ccf
Alexander Swensen <alex.swensen@gmail.com>
2017-11-04 16:55:09 -0500

    rearrange code and add docstrings

--- main.py
+++ main.py
@@ -11,26 +11,44 @@ from machine import Pin
 import machine
 import bme280
 import network
-# Setup external LED, PIR Sensor, and I2C bus
-led = Pin(4, Pin.OUT)
-ir = Pin(17, Pin.IN)
 
-# setup WIFI
+# sensor pins
+led_pin = 4
+pir_sensor_pin = 17
+
+# Wifi credentials
 wifi_ssid = "wifinetworkname" # wifinetworkname
 wifi_password = "wifipassword" # wifipassword
+
+# Setup external LED, PIR Sensor, and I2C bus
+led = Pin(led_pin, Pin.OUT)
+ir = Pin(pir_sensor_pin, Pin.IN)
+
+# setup WIFI
 station = network.WLAN(network.STA_IF)
 station.active(True)
 station.connect(wifi_ssid, wifi_password)
 
+
 def turn_led_on():
+    """
+    Turns the LED On
+    """
     led.value(1)
     print("The LED is on!")
 
 def turn_led_off():
+    """
+    Turns the LED Off
+    """
     led.value(0)
     print("The LED is off!")
 
-def checkConnection():
+def check_connection():
+    """
+    Will check for wifi connection.
+    If the wifi is not connected, it will attempt to connect until a connection is successful.
+    """
     connected = station.isconnected()
     if not connected:
         while not connected:
@@ -40,9 +58,9 @@ def checkConnection():
             connected = station.isconnected()
 
 while True:
-    checkConnection()
+    check_connection()
     if ir.value() == 1:
         turn_led_on()
     else: 
         turn_led_off()
-    sleep(0.2)
+    sleep(0.1)
