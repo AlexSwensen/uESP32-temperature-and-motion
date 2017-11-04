27cc5473a236498da62f0de8f09decc6072de879
Alexander Swensen <alex.swensen@gmail.com>
2017-11-04 16:21:01 -0500

    connect to network

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ led = machine.Pin(4, machine.Pin.OUT)
 ir = machine.Pin(17, machine.Pin.IN)
 
 # setup WIFI
+wifi_ssid = "wifinetworkname"
+wifi_password = "wifipassword"
 station = network.WLAN(network.STA_IF)
 station.active(True)
-station.connect("YourNetworkName", "YourNetworkPassword")
-
-
+station.connect(wifi_ssid, wifi_password)
 
 def turn_led_on():
     led.value(1)
@@ -21,7 +21,17 @@ def turn_led_off():
     led.value(0)
     print("The LED is off!")
 
+def checkConnection():
+    connected = station.isconnected()
+    if not connected:
+        while not connected:
+            print("Not connected, attempting to connect")
+            station.connect(wifi_ssid, wifi_password)
+            sleep(5)
+            connected = station.isconnected()
+
 while True:
+    checkConnection()
     if ir.value() == 1:
         turn_led_on()
     else: 
